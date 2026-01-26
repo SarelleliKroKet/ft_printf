6ca9dcc01c7aa5204a7a1f53c9966250e6cbced4
KroKet <147746852+SarelleliKroKet@users.noreply.github.com>
2026-01-26 15:03:26 +0300

    feat & fix: changes the following functions
    - added description to README file.
    - print_int file added to project.
    - removed old header include from all c files.

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 _This project has been created as part of the 42 curriculum by usolak_
 
-**Description**
+**Description**  
+This project aims the understand how variadic functions works and creating our own printf function.  
 
 **Instructions**
 
 **Resources**  
-For variadic functions  
-https://stackoverflow.com/questions/77300562/variadic-functions-in-c-concept  
-https://youtu.be/S-ak715zIIE?si=yyo3liv5_1-WCfGK  
-https://youtu.be/oDC208zvsdg?si=5HIRZntt5Ft6sZ65  
-Using AI for eplanation of variadic functions  
-
+For variadic functions:  
+https://stackoverflow.com/questions/77300562/variadic-functions-in-c-concept.  
+https://youtu.be/S-ak715zIIE?si=yyo3liv5_1-WCfGK.  
+https://youtu.be/oDC208zvsdg?si=5HIRZntt5Ft6sZ65.  
+Using AI for eplanation of variadic functions.  
--- ft_print_int.c
+++ ft_print_int.c
@@ -1,26 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_str.c                                     :+:      :+:    :+:   */
+/*   ft_print_int.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: usolak <usolak@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/26 13:54:34 by usolak            #+#    #+#             */
-/*   Updated: 2026/01/26 14:56:52 by usolak           ###   ########.fr       */
+/*   Created: 2026/01/26 13:59:26 by usolak            #+#    #+#             */
+/*   Updated: 2026/01/26 14:56:51 by usolak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_print_str(char *str)
+int	ft_print_int(int n)
 {
-	int		i;
-
-	i = 0;
-	while (str[i])
+	int	sum;
+	
+	sum = 0;
+	if (n == -2147483648)
 	{
-		write(1, &str[i], 1);
-		i++;
+		return (ft_print_str("-2147483648"));
 	}
-	return (i);
+	if (n < 0)
+	{
+		sum += ft_print_char('-');
+		n = -n;
+	}
+	if (n > 9)
+	{
+		sum += ft_print_int(n / 10);
+		sum += ft_print_int(n % 10);
+	}
+	else
+	{
+		sum += ft_print_char(n + 48);
+	}
+	return (sum);
 }
