46b777b48530bddbc949d524409df4be89357071
SarelleliKroKet <solak_umut@outlook.com>
2026-02-01 12:11:26 +0300

    test: git configration test

--- ft_printf.c
+++ ft_printf.c
@@ -6,18 +6,20 @@
 /*   By: usolak <usolak@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 10:14:01 by usolak            #+#    #+#             */
-/*   Updated: 2026/02/01 10:47:53 by usolak           ###   ########.fr       */
+/*   Updated: 2026/02/01 11:25:57 by usolak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <stdarg.h>
-static int sum(const char *s, ...)
+#include "ft_printf.h"
+
+static int	sum(const char *s, ...)
 {
-	va_list akosaca;
-	va_start(akosaca, s);
+	va_list	akosaca;
+	va_start (akosaca, s);
+
 	int		i;
 	i = 0;
-	int yaycicek;
+	int		yaycicek;
 	yaycicek = va_arg(akosaca, int);
 	while (s[i])
 	{
@@ -36,9 +38,10 @@ static int sum(const char *s, ...)
 	va_end(akosaca);
 	return (yaycicek);
 }
+
 #include <stdio.h>
 
-int main()
+int	main()
 {
 	int val = sum("+*-/%%", 5, 5, 10, 5, 5, 15);
 	printf("%d\n", val);
